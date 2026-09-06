892c5511e332b037c597569a7169e50c86f1fbcd
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2026-09-06 20:27:08 +0200

    Add Think Tank config

---
+++ config.js
@@ -0,0 +1,7 @@
+window.THINK_TANK_CONFIG = {
+  // Paste the deployed Google Apps Script Web App URL here.
+  // Leave blank to run the frontend in demo mode.
+  apiUrl: '',
+  siteUrl: 'https://hackejandro.github.io/eu-media-poll/',
+  timeZone: 'Europe/Brussels'
+};
