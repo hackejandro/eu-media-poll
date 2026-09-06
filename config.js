e552d0bc1eddf57b66d71c24e26f90b2bf298906
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2026-09-06 20:47:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.THINK_TANK_CONFIG = {
   // Paste the deployed Google Apps Script Web App URL here.
   // Leave blank to run the frontend in demo mode.
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/a/macros/euobserver.com/s/AKfycbx0vorCrGzUkUdxJDI4Hpw4Z6GGanYLwmlCKq335w8ysWrFG7S18d_DZs54fHNgE86j/exec',
   siteUrl: 'https://hackejandro.github.io/eu-media-poll/',
   timeZone: 'Europe/Brussels'
 };
